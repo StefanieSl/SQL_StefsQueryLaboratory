9f9ebf4d4eff1f4f4a77bf6d943c2aeb07262445
unknown <s.sluschny@web.de>
2025-03-06 19:52:00 +0100

    add 2025 books

--- Books.sql
+++ Books.sql
@@ -1,10 +1,28 @@
 CREATE DATABASE books;
 
 CREATE TABLE Books (
-	BuchID int,
+	BuchID int NOT NULL AUTO_INCREMENT,
+    Titel varchar(255),
     Autor varchar(255),
     Genre varchar(255),
-    Rating int
+    grRating float,
+    MyRating float,
+    PRIMARY KEY (BuchID)
 );
+
+INSERT INTO Books (Titel, Autor, grRating, MyRating)
+VALUES
+('Missing You', 'Harlan Coben', 4.0, 3.0);
+
+UPDATE Books
+SET Genre = 'Thriller'
+WHERE BuchID = 1;
+
+INSERT INTO Books (Titel, Autor, Genre, grRating, MyRating)
+VALUES
+("The Housemaid's Secret", 'Freida McFadden', 'Thriller', 4.22, 3.0),
+("Safe", 'S.K. Barnett', 'Thriller', 3.9, 4.0),
+("We Used to Live Here", 'Marcus Kliewer', 'Horror', 3.71, 5.0),
+("What the Woods Took", 'Courtney Gould', 'Horror', 3.91, 3.0);
 
 SELECT * FROM Books;
